8c4d858780a8fadac806c4a73a4cddd5d74add36
Epicgamer30001 <alvinpark7412@gmail.com>
2026-06-11 10:50:56 -0700

    yay

--- hello_world.py
+++ hello_world.py
@@ -3,4 +3,28 @@ import time
 
 def main():
     #create camera object 
+    zed = sl.Camera()
+
+    #create Init parametsrs object and set parameters 
+    init_params = sl.InitParameters()
+    init_params.sdk_verbose = 0 
+    init_params.camera_resolution = sl.RESOLUTION.HD1080
+    init_params.camera_fps = 30
+
+    #open camera with parameters specified
+    err = zed.open(init_params)
+    if err != sl.ERROR_CODE.SUCCESS:
+        exit(1)
     
+    #get camera info 
+    zed_serial = zed.get_camera_information().serial_number
+    print(f"Yooooooo serial number ->:  {zed_serial}")
+
+    time.sleep(3)
+
+    #close the camera 
+    zed.close()
+
+if __name__ == "__main__":
+    main()
+
